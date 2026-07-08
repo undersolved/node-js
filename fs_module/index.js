43a0d5a21dac87ef61a03afbee7a2973d795c43a
Undersolved <singh.bhupendra.h@gmail.com>
2026-07-08 09:40:23 +0530

    git ignore update

--- fs_module/index.js
+++ fs_module/index.js
@@ -1,1 +1,10 @@
 const fs = require('fs');
+
+
+// echo "# node-js" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/undersolved/node-js.git
+// git push -u origin main
