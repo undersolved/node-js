09a45c20746ccc68320177d8bc07c8a658533df4
Undersolved <singh.bhupendra.h@gmail.com>
2026-07-09 10:01:46 +0530

    custom server modified

--- custom_server_1/server.js
+++ custom_server_1/server.js
@@ -1,1 +1,10 @@
-const http = require('node"http');
+const http = require("node:http");
+
+const server = http.createServer((req, res) => {
+	console.log(`I got an incoming request at ${Date.now()}`);
+	res.end(`Response sent at ${Date.now()}`);
+});
+
+server.listen(8000, () => {
+	console.log("server is up on PORT 8000");
+});
