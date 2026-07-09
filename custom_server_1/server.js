509251c35a5a06296f17e9d7771982f3f63c10cd
Undersolved <singh.bhupendra.h@gmail.com>
2026-07-09 10:20:41 +0530

    safety

--- custom_server_1/server.js
+++ custom_server_1/server.js
@@ -2,7 +2,9 @@ const http = require("node:http");
 
 const server = http.createServer((req, res) => {
 	console.log(`I got an incoming request at ${Date.now()}`);
-	res.end(`Response sent at ${Date.now()}`);
+	console.log(req.headers);
+	res.writeHead(200);
+	res.end(`Request ended at ${Date.now()}`);
 });
 
 server.listen(8000, () => {
