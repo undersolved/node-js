d46f5c22ec6a8157d111c981b11982c629793883
Undersolved <singh.bhupendra.h@gmail.com>
2026-07-10 13:02:51 +0530

    new routes updated

--- custom_server_1/server.js
+++ custom_server_1/server.js
@@ -2,9 +2,17 @@ const http = require("node:http");
 
 const server = http.createServer((req, res) => {
 	console.log(`I got an incoming request at ${Date.now()}`);
-	console.log(req.headers);
+	// console.log(req.headers);
 	res.writeHead(200);
-	res.end(`Request ended at ${Date.now()}`);
+
+	switch (req.url) {
+		case "/":
+			return res.end("you are on the homepage");
+		case "/about":
+			return res.end("you are at the about page");
+		case "/contact us":
+			return res.end("you are at contact us page");
+	}
 });
 
 server.listen(8000, () => {
