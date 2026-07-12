5d4d9209cd18d61edbb4ce6b61a2f24f936ca1a9
Undersolved <singh.bhupendra.h@gmail.com>
2026-07-12 10:18:59 +0530

    server.js updated

--- custom_server_1/server.js
+++ custom_server_1/server.js
@@ -3,15 +3,20 @@ const http = require("node:http");
 const server = http.createServer((req, res) => {
 	console.log(`I got an incoming request at ${Date.now()}`);
 	// console.log(req.headers);
-	res.writeHead(200);
 
 	switch (req.url) {
 		case "/":
+			res.writeHead(200);
 			return res.end("you are on the homepage");
 		case "/about":
+			res.writeHead(200);
 			return res.end("you are at the about page");
-		case "/contact us":
+		case "/contact-us":
+			res.writeHead(200);
 			return res.end("you are at contact us page");
+		default:
+			res.writeHead(404);
+			return res.end("You are Lost");
 	}
 });
 
